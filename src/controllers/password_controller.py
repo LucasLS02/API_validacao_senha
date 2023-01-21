b004632629316e6e1c0a0f2b90717c72e8872b88
Lucas Silva <lucas@MacBook-Pro-2.local>
2023-01-21 15:16:21 -0300

    Add verification for minSize
    This update includes the addition of the verify endpoint logic
    to validate the password lenght.

--- src/controllers/password_controller.py
+++ src/controllers/password_controller.py
@@ -2,8 +2,7 @@ from flask import Blueprint, request
 
 from marshmallow.exceptions import ValidationError
 
-from src.schemas.password_schema import PasswordSchema
-
+from src.schemas.password_schema import PasswordSchema, PasswordVerificationResponseSchema
 
 password_controller = Blueprint('password_controller', __name__)
 
@@ -11,12 +10,41 @@ password_controller = Blueprint('password_controller', __name__)
 @password_controller.route('/verify', methods=['POST'])
 def verify():
     try:
-
         data = PasswordSchema().load(request.json)
 
+        response = {
+            'verify': True,
+            'noMatch': []
+        }
+
+        for rule in data['rules']:
+            match rule['rule']:
+                case 'minSize':
+                    if len(data['password']) > rule['value']:
+                        response['noMatch'].append('minSize')
+                        response['verify'] = False
+
+                case 'minUppercase':
+                    response['verify'] = False
+
+                case 'minLowercase':
+                    response['verify'] = False
+
+                case 'minDigit':
+                    response['verify'] = False
+
+                case 'minSpecialChars':
+                    response['verify'] = False
+
+                case 'noRepeted':
+                    response['verify'] = False
+
+        response = PasswordVerificationResponseSchema().dump(response)
+
     except ValidationError as e:
-        return {'errors': e.messages}
+        return {'error': e.messages}, 422
     except Exception as e:
         print(e)
+        return {'error': 'Internal Server Error'}, 500
 
-    return {'data': data}, 200
+    return {'data': response}, 200
